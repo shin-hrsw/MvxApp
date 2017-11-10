2a5a2f0e17fe2a3c39baa2d6296c85ab69fb60fc
Shintaro Hirasawa <nobizo@gmail.com>
2017-11-10 15:42:03 +0900

    ToDoクラスを追加

---
+++ MvxApp/MvxApp/Models/ToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MvxApp.Core.Models
+{
+    public class ToDo
+    {
+        #region プロパティ
+        public DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+        #endregion
+
+        #region コンストラクタ
+        public ToDo()
+        {
+            this.Date = DateTime.Today;
+            this.Title = string.Empty;
+            this.Text = string.Empty;
+        }
+        #endregion
+    }
+}
