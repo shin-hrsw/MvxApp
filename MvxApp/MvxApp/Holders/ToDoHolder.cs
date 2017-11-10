f2d9f513d003391b912db8559fa72584a869df7b
Shintaro Hirasawa <nobizo@gmail.com>
2017-11-10 16:17:27 +0900

    ToDo保存クラスを追加

---
+++ MvxApp/MvxApp/Holders/ToDoHolder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvxApp.Core.Holders
+{
+    public class ToDoHolder
+    {
+        #region プロパティ
+        private static ToDoHolder _instance = new ToDoHolder();
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        public static ToDoHolder Current { get => _instance; }
+
+        private List<Models.ToDo> _list = new List<Models.ToDo>();
+        /// <summary>
+        /// ToDoリスト
+        /// </summary>
+        public List<Models.ToDo> ToDoList { get => this._list; }
+        #endregion
+
+        #region コンストラクタ
+        // Singletonなのでコンストラクタはprivateにしておく
+        private ToDoHolder() { }
+        #endregion
+    }
+}
