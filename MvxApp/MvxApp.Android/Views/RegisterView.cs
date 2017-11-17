5719822dd7fdb16a97628f6afe8a9fca8d3d53c3
Shintaro Hirasawa <nobizo@gmail.com>
2017-11-17 10:49:59 +0900

    登録用のActivityを追加

---
+++ MvxApp/MvxApp.Android/Views/RegisterView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+
+namespace MvxApp.Droid.Views
+{
+    [Activity(Label = "RegisterView")]
+    public class RegisterView : MvxActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.RegisterView);
+        }
+    }
+}
