4b22aea7d5836d0520c6e07dde639ae135233494
Shintaro Hirasawa <nobizo@gmail.com>
2017-11-17 09:43:49 +0900

    登録用ViewModelを追加

---
+++ MvxApp/MvxApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+
+namespace MvxApp.Core.ViewModels
+{
+    public class RegisterViewModel : MvxViewModel
+    {
+        #region プロパティ
+        public DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+        #endregion
+
+        #region コンストラクタ
+        public RegisterViewModel()
+        {
+            this.Date = DateTime.Today;
+            this.Title = string.Empty;
+            this.Text = string.Empty;
+        }
+        #endregion
+    }
+}
